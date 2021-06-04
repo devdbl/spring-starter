99af29ea35ad79add94d1f05d960c41579cf84c5
DBL <dominic.blattner@stud.abbts.ch>
2021-06-04 14:33:43 +0200

    first commit

---
+++ src/main/java/ch/abbts/ndsswe/swd/springstarter/ActionsTest.java
@@ -0,0 +1,9 @@
+package ch.abbts.ndsswe.swd.springstarter;
+
+
+public class ActionsTest {
+    private void print(){
+        System.out.println("Hello world!");
+    }
+
+}
---
+++ src/test/java/ch/abbts/ndsswe/swd/springstarter/SpringstarterApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.abbts.ndsswe.swd.springstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
